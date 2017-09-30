8ca36d78add88409c9bf12b004d664f6245c4bbf
Guayisimo5 <guayisimo5@gmail.com>
2017-09-30 23:00:35 +0800

    first refactoring TSV

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -2,18 +2,22 @@
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
+
+  def structure_data(line)
+    row = Hash.new
+    line_splitted = line.split("\t")
+    line_splitted.each_with_index do |col, i|
+      row[@header[i]] = col
+    end
+    return row
+  end
   def take_tsv(tsv)
     rows = tsv.split("\n")
-    header = rows.first.split("\t")
+    @header = rows.first.split("\t")
     @data = []
     rows.each_with_index  do |line, index|
       next if index.zero?
-      row = Hash.new
-      line_splitted = line.split("\t")
-      line_splitted.each_with_index do |col, i|
-        row[header[i]] = col
-      end
-      @data.push(row)
+      @data << structure_data(line)
     end
     @data
   end
