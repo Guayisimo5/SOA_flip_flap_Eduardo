3466b1d01b78a198742671d16fc9d421ec24691c
Guayisimo5 <guayisimo5@gmail.com>
2017-09-30 23:51:33 +0800

    RF 2 to_tsv RF 3 Take_tsv

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -3,30 +3,51 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
 
-  def structure_data(line)
+  def structure_data(line,titles)
     row = Hash.new
     line_splitted = line.split("\t")
     line_splitted.each_with_index do |col, i|
-      row[@header[i]] = col
+      row[titles[i]] = col
     end
     return row
   end
+
+  def get_headers_string(data)
+    data.first.split("\t")
+  end
+
+  def structure_string(split_info,header)
+    split_info.each_with_index  do |line, index|
+      next if index.zero?
+      @data << structure_data(line,header)
+    end
+  end
+
   def take_tsv(tsv)
     rows = tsv.split("\n")
-    @header = rows.first.split("\t")
+    header = get_headers_string(rows)
     @data = []
-    rows.each_with_index  do |line, index|
-      next if index.zero?
-      @data << structure_data(line)
-    end
+    structure_string(rows,header)
     @data
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
+
+  def get_headers_data_structured(data)
+    heads = data.first.keys.join("\t") << "\n"
+    heads
+  end
+
+  def string_the_data(data)
+    data_string =""
+    data.each { |line| data_string << line.values.join("\t") << "\n" }
+    data_string
+  end
+
   def to_tsv
-    output = @data.first.keys.join("\t") << "\n"
-    @data.each { |line| output << line.values.join("\t") << "\n" }
-    output
+    output_string = get_headers_data_structured(@data)
+    output_string << string_the_data(data)
+    output_string
   end
 end
